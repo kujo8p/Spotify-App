52a0ca4aa3247adbdd2068f809406ffd186ee7e9
Kurt <kujo8p@gmail.com>
2023-07-16 14:42:55 -0500

    deletion of uness code

--- spotifyapp/main_app/views.py
+++ spotifyapp/main_app/views.py
@@ -92,33 +92,3 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-# class AuthURL(APIView):
-#   def get(self, request, format=None):
-#     scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing user-library-read user-read-private'
-
-#     url = Request('GET', 'https://accounts.spotify.com/authorize', params={
-#       'scope': scopes,
-#       'response_type': 'code',
-#       'redirect_uri': REDIRECT_URI,
-#       'client_id': CLIENT_ID
-#     }).prepare().url
-
-#     return Response({'url': url}, status=status.HTTP_200_OK)
-
-# def spotify_callback(request, format=None):
-#   code = request.GET.get('code')
-#   error = request.GET.get('error')
-
-#   response = post('https://accounts.spotify.com/api/token', data={
-#     'grant_type': 'authorization_code',
-#     'code': code,
-#     'redirect_uri': REDIRECT_URI,
-#     'client_id': CLIENT_ID,
-#     'client_secret': CLIENT_SECRET
-#   }).json()
-
-#   access_token = response.get('access_token')
-#   token_type = response.get('token_type')
-#   refresh_token = response.get('refresh_token')
-#   expires_in = response.get('expires_in')
-#   error = response.get('error')
