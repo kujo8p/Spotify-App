a80db8b51a5f8aaf21f1b1b26f1042bff53b39f2
Kurt <kujo8p@gmail.com>
2023-07-17 14:23:54 -0500

    break point

--- spotifyapp/main_app/urls.py
+++ spotifyapp/main_app/urls.py
@@ -9,7 +9,13 @@ urlpatterns = [
     path("artist/create/", views.ArtistCreate.as_view(), name='artist_create'),
     path("song/create/", views.SongCreate.as_view(), name='song_create'),
     path("playlist/create/", views.PlaylistCreate.as_view(), name='playlist_create'),
+
+
+
     path("playlist/<int:playlist_id>/", views.playlist_detail, name='playlist_detail'),
+
+
+    
     path('playlist/<int:pk>/delete', views.PlaylistDelete.as_view(), name='playlist_delete'),
     path('<int:playlist_id>/assoc_song/<int:song_id>/', views.assoc_song, name="assoc_song"),
     path('<int:playlist_id>/unassoc_song/<int:song_id>/', views.unassoc_song, name="unassoc_song"),
